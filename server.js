b5ea342ed5b4706d13ada48c3009945a2b81592b
Jaideep Singh <jaisidhu2004@gmail.com>
2024-03-26 20:39:56 +0530

    26/3/24

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
